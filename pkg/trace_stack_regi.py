f99c490bccfd93b3737c4890a805d0da4c99e87f
Yongjae <whdydwo8@gmail.com>
2022-03-07 20:00:21 +0900

    complete registration

--- pkg/trace_stack_regi.py
+++ pkg/trace_stack_regi.py
@@ -1,13 +1,18 @@
-import gzip, os, re, pickle
+import gzip, os, re, pickle, sys
+from shutil import copyfile
 from inspect import trace
 import xml.etree.ElementTree as ET
 from collections import Counter
 from typing import List, Set, Dict, Tuple
+from importlib_metadata import metadata
 from matplotlib import docstring
 from skimage import io
 import numpy as np
+from pystackreg import StackReg
+from pystackreg.util import to_uint16
 
-from pkg.utils import CustomLogger
+sys.path.append('./pkg')
+from utils import CustomLogger
 
 logger = CustomLogger().info_logger
 
@@ -113,7 +118,7 @@ def unique_counter(fnames: list, save_name: bool=True) -> Dict:
                 unique_names[img_id] = {'day' : [], 'count' : 0, 'fname' : []}
 
             if parsed_name.group('day') not in unique_names[img_id]['day']:
-                unique_names[img_id]['day'].append(int(parsed_name.group('day')))
+                unique_names[img_id]['day'].append(parsed_name.group('day')) # day type is str
                 unique_names[img_id]['count'] += 1
                 if save_name:
                     unique_names[img_id]['fname'].append(parsed_name.group('fname'))
@@ -134,7 +139,7 @@ def day_sorting(fname: str):
     parsed_name = int(parser.search(fname).group('day')) # sorting list in ascending order based on the day
     return parsed_name
 
-def search_regi_files(raw_path: str='./data', regi_path: str='./registration') -> Dict:
+def search_regi_files(raw_path: str='./data', crop_path: str='./crop_data', regi_path: str='./registration') -> Dict:
     '''
     Input Args:
         raw_path: str (default = './data')
@@ -149,37 +154,40 @@ def search_regi_files(raw_path: str='./data', regi_path: str='./registration') -
             example) {'#raw_file_id' :[(raw_file_1.tif, raw_file_1.traces), (raw_file_2.tif, raw_file_2.traces), ...]}
     '''
     dir_exist_check(raw_path) # check raw data directory existence
+    dir_exist_check(crop_path) # check registration directory existence
     dir_exist_check(regi_path) # check registration directory existence
 
     logger.info('Directory existence check complete')
     
     raw_list = fname_extension_check(raw_path, '.traces')
     raw_list = unique_counter(raw_list, save_name=True)
-    regi_list = fname_extension_check(regi_path, '.traces')
-    regi_list = unique_counter(regi_list, save_name=False)
 
     logger.debug(f'Raw uniqe count : {raw_list}')
-    logger.debug(f'Regi uniqe count : {regi_list}')
 
     # extract file list that is not yet preformed registration
     logger.debug('start extract file list for registration')
     file_path_regi = {}
     for raw_id in raw_list.keys(): 
+        # crop check. if the day is smaller than 2, crop is can't be performed
         if raw_list[raw_id]['count'] < 2:
             logger.debug(f'More than 2 files are needed for registration. {raw_id} is excluded')
             continue
+        
+        # metadata of cropped image
+        # if cropping is performed, pass and check next image 
+        if not os.path.isfile(os.path.join(crop_path, f'#{raw_id}', f'#{raw_id} meta.pickle')): 
+            continue
+        else:
+            metadata = load_pickle(os.path.join(crop_path, f'#{raw_id}'), f'#{raw_id} meta.pickle')
 
-        if raw_id in regi_list.keys(): ##################################### crop metadata로 확인하는것으로 바꾸기
-            if set(raw_list[raw_id]['day']) == set(regi_list[raw_id]['day']):
-                logger.debug(f'Already registration is preformed. {raw_id} is excluded')
+            if set(raw_list[raw_id]['day']) == set(metadata['day']):
+                logger.debug(f'Already image cropping is preformed. #{raw_id} is excluded')
                 continue
 
         file_path_regi[raw_id] = []
         sorted_fnames = [fname.strip('.traces') for fname in sorted(raw_list[raw_id]['fname'], key=day_sorting)]
         file_path_regi[raw_id] = [(fname+'.tif', fname+'.traces') for fname in sorted_fnames]
-
-        logger.debug(f'{raw_list[raw_id]["fname"]} is added to the registration list')
-
+        logger.debug(f'{raw_list[raw_id]["fname"]} is added to the image crop list')
     return file_path_regi
 
 def path_to_array(path: Dict) -> np.ndarray:
@@ -196,7 +204,7 @@ def path_to_array(path: Dict) -> np.ndarray:
     path_array = np.array([path['x'], path['y'], path['z']])
     return path_array
 
-def edge_indexing(size_single_axis: int, boundary_pixel_idx: Tuple[int], margin_pixel: int=10) -> Tuple[int]:
+def edge_indexing(size_single_axis: int, boundary_pixel_idx: Tuple[int], margin_pixel: int=20) -> Tuple[int]:
     '''
     Input Args:
         size_single_axis: int
@@ -228,7 +236,7 @@ def edge_indexing(size_single_axis: int, boundary_pixel_idx: Tuple[int], margin_
     return (lowerbnd_idx, uppperbnd_idx)
 
 
-def crop_img(img: np.ndarray, coords_idx: Tuple[int], margin_pixel: int=10) -> Tuple[np.ndarray, Tuple[int]]:
+def crop_img(img: np.ndarray, coords_idx: Tuple[int], margin_pixel: int=20) -> Tuple[np.ndarray, Tuple[int]]:
     '''
     Input Args:
         img: np.ndarray [z, y, x]
@@ -291,7 +299,7 @@ def load_pickle(path: str, fname: str):
     with open(os.path.join(path, fname), 'rb') as f:
         return pickle.load(f)
 
-def segment_crop(file_path_regi: Dict[str, List[Tuple]], raw_path: str='./data', crop_path: str='crop_data', margin_pixel: int=10):
+def segment_crop(file_path_regi: Dict[str, List[Tuple]], raw_path: str='./data', crop_path: str='crop_data', margin_pixel: int=20):
     '''
     Input Args:
         file_path_regi: Dict[str : List[Tuple]]
@@ -304,9 +312,13 @@ def segment_crop(file_path_regi: Dict[str, List[Tuple]], raw_path: str='./data',
         margin_pixel: int (default=10)
             the No. of pixel for margin
     '''
-    logger.debug('start segment_registration')
+    logger.debug('start segment cropping')
     name_parser = define_name_parser()
     
+    if len(file_path_regi.keys())==0: # if no image to be cropped, break this function
+        logger.debug(f'There is no image to be cropped. pass the crop.')
+        return 
+
     for img_id in file_path_regi.keys(): # iterate file id
         logger.debug(f'img id : {img_id}')
 
@@ -344,7 +356,7 @@ def segment_crop(file_path_regi: Dict[str, List[Tuple]], raw_path: str='./data',
                             seg[2, :].min(), seg[2, :].max()) # zmin, zmax
 
                 # image crop and index for cropped image
-                cropped_img, new_bnd_idx = crop_img(img, rect_idx, margin_pixel)
+                cropped_img, new_bnd_idx = crop_img(img, rect_idx, margin_pixel=15)
 
                 # save cropped image
                 io.imsave(os.path.join(crop_dir, f'd{seg_day} {segment_name}.tif'), cropped_img)
@@ -372,6 +384,9 @@ def segment_crop(file_path_regi: Dict[str, List[Tuple]], raw_path: str='./data',
     segment_counter = Counter(metadata['segment_name'])
     metadata['segment_name'] = [segment for segment in segment_counter.keys() if segment_counter[segment] > 1] # save the segment name if count is larger than 1
     
+    # sorting day information
+    metadata['day'] = sorted(metadata['day'], key = lambda x: int(x))
+
     # save the common largest image size for registration
     for segment in metadata.keys():
         if segment not in ['day', 'segment_name']: # exclude day and segment name keyword
@@ -431,22 +446,154 @@ def pad_image(img: np.ndarray, regi_size: np.ndarray) -> np.ndarray:
     return padded_image
 
 
-def regi_cropped_img(crop_path: str='./crop_data'):
+def pixel_zcorr(ref: np.ndarray, mov: np.ndarray) -> np.ndarray:
+    '''
+    z axis Pearson's correlation coefficient r for each pixel
+
+    Parameters
+    ----------
+    ref : np.ndarray (3D)
+        reference image
+    mov : np.ndarray (3D)
+        image to be transformed
+
+    Returns
+    -------
+    pixel_z_pearsonr : np.ndarray
+        pixel-wise z axis Pearson's correlation coefficient
+    '''
+    assert (type(ref)==np.ndarray) and (type(mov)==np.ndarray), f'the input array should be np.ndarray, but ref is {type(ref)}D and mov is {type(mov)}'
+    assert (ref.ndim==3) and (mov.ndim==3), f'the input array dimension should be 3, but ref is {ref.ndim}D and mov is {mov.ndim}'
+    ref = ref - ref.mean(axis=0)
+    mov = mov - mov.mean(axis=0)
+    pixel_z_pearsonr = (ref*mov).sum(axis=0) / (np.sqrt(((ref)**2+sys.float_info.min).sum(axis=0))) / (np.sqrt((mov**2+sys.float_info.min).sum(axis=0)))
+    return pixel_z_pearsonr
+
+def stack_registration(img_stack: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
+    '''
+    registration of projected image to match x, y position
+
+    Parameters
+    ----------
+    img_stack : Dict[str, np.ndarray]
+        image stack of different day
+
+    Returns
+    -------
+    img_stack : Dict[str, np.ndarray]
+        registration of image stack
+    '''
+    for idx, day in enumerate(img_stack.keys()):
+        if idx==0:
+            ref_day = day
+            ref = img_stack[day] # reference image for registration of projection image
+        else:
+            transformed = img_stack[day]
+            
+            # x, y registration
+            sr = StackReg(StackReg.RIGID_BODY)
+            sr.register(ref.max(axis=0), transformed.max(axis=0)) # register 2nd image to 1st image in x, y position
+            for z_idx, zimg in enumerate(transformed):
+                transformed[z_idx] = to_uint16(sr.transform(zimg)) # apply transform and clip negative value
+            
+            # z axis registration
+            transformed = z_axis_regi(ref, transformed)
+            img_stack[day] = transformed
+    return img_stack
+
+def z_axis_regi(ref_stack: np.ndarray, mov_stack: np.ndarray, zshift: int=80) -> np.ndarray:
+    '''
+    _summary_
+
+    Parameters
+    ----------
+    ref_stack : np.ndarray
+        reference image stack for registration
+    mov_stack : np.ndarray
+        image stack to be transformed in z axis
+    zshift : int, optional
+        z axis shift range for z axis registration, by default 10
+
+    Returns
+    -------
+    np.ndarray
+        z axis shifted image stack
+    '''
+    assert (type(ref_stack)==np.ndarray) and (type(mov_stack)==np.ndarray), f'the input array should be np.ndarray, but ref is {type(ref_stack)}D and mov is {type(mov_stack)}'
+    assert (ref_stack.ndim==3) and (mov_stack.ndim==3), f'the input array dimension should be 3, but ref is {ref_stack.ndim}D and mov is {mov_stack.ndim}'
+    
+    # z correlation for each pixel. z_corr(x, y) z_series(x, y)
+    optimal_shift = 0
+    max_score = 0
+
+    # if the zshift is larger than the z axis length, z shift is the length of z axis
+    zlen = ref_stack.shape[0] # z axis length
+    if zshift > zlen:
+        zshift = zlen
+
+    for shift in range(zshift):
+        ref = ref_stack[:zlen-shift, :, :]
+        z_mov = mov_stack[shift:, :, :]
+
+        z_corr = pixel_zcorr(ref, z_mov)
+        scoring = np.mean(z_corr) / (sys.float_info.min + np.std(z_corr))
+        # logger.debug(f'z shift score : {scoring} | z_corr mean : {np.mean(z_corr)} | z_corr std : {np.std(z_corr)}')
+
+        if max(scoring, max_score)==scoring:
+            max_score = scoring
+            optimal_shift = shift
+            
+    logger.debug(f'Optimal shift : {shift} | max_zcorr_score : {max_score}')
+    z_mov = np.zeros_like(mov_stack)
+    z_mov[:zlen - optimal_shift, :, :] = mov_stack[optimal_shift:, :, :]
+    return z_mov
+
+def regi_cropped_img(crop_path: str='./crop_data', regi_path: str='./registration'):
+    if len(os.listdir(crop_path))==0: # if there is no cropped image
+        logger.debug('The is no cropped image for registration. Do the image crop first')
+        return
+
     for img_id in os.listdir(crop_path): # get cropped image list
         segment_meta = load_pickle(os.path.join(crop_path, img_id), f'{img_id} meta.pickle')
-        
-        for seg in segment_meta['segment_name']:
-            common_img_size = segment_meta[seg]
-            img_stack = []
+        dir_exist_check(os.path.join(regi_path, img_id)) # check directory
 
-            for day in segment_meta['day']:
-                img = io.imread(os.path.join(crop_path, img_id, f'd{day} {seg}.tif'))
-                logger.debug(f'[{seg} segment] day {day} | img shape : {img.shape} | common image size : {common_img_size}')
+        # registration check
+        # if the metadata is not exist in the registration directory, perform registration
+        # else pass and check the next image
+        if not os.path.isfile(os.path.join(crop_path, f'{img_id}', f'{img_id} meta.pickle')):
+            logger.debug(f'[{img_id}] {img_id} meta.pickle is not exist. start registration')
 
-                # padding image
-                padded_img = pad_image(img, common_img_size)
-                logger.debug(f'padded image size : {padded_img.shape}')
-                img_stack.append(padded_img)
+            for seg in segment_meta['segment_name']:
+                common_img_size = segment_meta[seg]
+                img_stack = {}
+
+                for day in segment_meta['day']:
+                    img = io.imread(os.path.join(crop_path, img_id, f'd{day} {seg}.tif'))
+                    logger.debug(f'[{seg} segment] day {day} | img shape : {img.shape} | common image size : {common_img_size}')
+
+                    # padding image
+                    padded_img = pad_image(img, common_img_size)
+                    logger.debug(f'padded image size : {padded_img.shape}')
+                    
+                    # collect img stack
+                    img_stack[day] = padded_img
+
+                # registration
+                logger.debug(f'[{seg} segment] day {day}')
+                img_stack = stack_registration(img_stack)
+                stacks = np.stack([img_stack[key] for key in img_stack.keys()], axis=1)
                 
+                # image stack save
+                io.imsave(os.path.join(regi_path, img_id, f'{seg}.tif'), stacks, metadata={'axes' : 'ZCYX'}, imagej=True)
+
+            # save metadata to registration directory
+            save_pickle(segment_meta, os.path.join(regi_path, img_id), f'{img_id} meta.pickle')
+        
+        else:
+            logger.debug(f'#{img_id} registration is already performed. pass and check the next image')
+            continue
+
 if __name__=='__main__':
     file_path_regi = search_regi_files(raw_path='./data', regi_path='./registration')
+    segment_crop(file_path_regi)
+    regi_cropped_img()
